096234dfb9a74828b47665a2f48fbb0a7758e5f9
damacek <damacek@cisco.com>
2023-12-18 13:42:00 +0100

    More Arcing

--- regex-automata/src/meta/strategy.rs
+++ regex-automata/src/meta/strategy.rs
@@ -79,7 +79,6 @@ pub(super) fn new(
     info: &RegexInfo,
     hirs: &[&Hir],
 ) -> Result<Arc<dyn Strategy>, BuildError> {
-    /*
     // At this point, we're committed to a regex engine of some kind. So pull
     // out a prefilter if we can, which will feed to each of the constituent
     // regex engines.
@@ -151,8 +150,8 @@ pub(super) fn new(
         debug!("skipping literal extraction since prefilters were disabled");
         None
     };
-     */
-    //let mut core = Arc::new(Core::new(info.clone(), None, hirs)?);
+
+    let mut core = Arc::new(Core::new(info.clone(), None, hirs)?);
     // Now that we have our core regex engines built, there are a few cases
     // where we can do a little bit better than just a normal "search forward
     // and maybe use a prefilter when in a start state." However, these cases
@@ -162,7 +161,6 @@ pub(super) fn new(
     // might give up or quit for reasons. If we had, e.g., a PikeVM that
     // supported reverse searching, then we could avoid building a full Core
     // engine for this case.
-    /*
     core = match ReverseAnchored::new(core) {
         Err(core) => core,
         Ok(ra) => {
@@ -185,9 +183,7 @@ pub(super) fn new(
         }
     };
     debug!("using core strategy");
-     */
-    //Ok(core)
-    panic!()
+    Ok(core)
 }
 
 #[derive(Clone, Debug)]
@@ -451,10 +447,10 @@ struct Core {
     nfa: NFA,
     nfarev: Option<NFA>,
     pikevm: wrappers::PikeVM,
-    backtrack: wrappers::BoundedBacktracker,
-    onepass: wrappers::OnePass,
-    hybrid: wrappers::Hybrid,
-    dfa: wrappers::DFA,
+    backtrack: Arc<wrappers::BoundedBacktracker>,
+    onepass: Arc<wrappers::OnePass>,
+    hybrid: Arc<wrappers::Hybrid>,
+    dfa: Arc<wrappers::DFA>,
 }
 
 impl Core {
@@ -482,62 +478,75 @@ impl Core {
         // match. (Construction can also fail if the NFA was compiled without
         // captures, but we always enable that above.)
         let pikevm = wrappers::PikeVM::new(&info, pre.clone(), &nfa)?;
-        let backtrack =
-            wrappers::BoundedBacktracker::new(&info, pre.clone(), &nfa)?;
+        let backtrack = Arc::new(wrappers::BoundedBacktracker::new(
+            &info,
+            pre.clone(),
+            &nfa,
+        )?);
         // The onepass engine can of course fail to build, but we expect it to
         // fail in many cases because it is an optimization that doesn't apply
         // to all regexes. The 'OnePass' wrapper encapsulates this failure (and
         // logs a message if it occurs).
-        let onepass = wrappers::OnePass::new(&info, &nfa);
+        let onepass = Arc::new(wrappers::OnePass::new(&info, &nfa));
         // We try to encapsulate whether a particular regex engine should be
         // used within each respective wrapper, but the DFAs need a reverse NFA
         // to build itself, and we really do not want to build a reverse NFA if
         // we know we aren't going to use the lazy DFA. So we do a config check
         // up front, which is in practice the only way we won't try to use the
         // DFA.
-        let (nfarev, hybrid, dfa) =
-            if !info.config().get_hybrid() && !info.config().get_dfa() {
-                (None, wrappers::Hybrid::none(), wrappers::DFA::none())
+        let (nfarev, hybrid, dfa) = if !info.config().get_hybrid()
+            && !info.config().get_dfa()
+        {
+            (
+                None,
+                Arc::new(wrappers::Hybrid::none()),
+                Arc::new(wrappers::DFA::none()),
+            )
+        } else {
+            // FIXME: Technically, we don't quite yet KNOW that we need
+            // a reverse NFA. It's possible for the DFAs below to both
+            // fail to build just based on the forward NFA. In which case,
+            // building the reverse NFA was totally wasted work. But...
+            // fixing this requires breaking DFA construction apart into
+            // two pieces: one for the forward part and another for the
+            // reverse part. Quite annoying. Making it worse, when building
+            // both DFAs fails, it's quite likely that the NFA is large and
+            // that it will take quite some time to build the reverse NFA
+            // too. So... it's really probably worth it to do this!
+            let nfarev = thompson::Compiler::new()
+                // Currently, reverse NFAs don't support capturing groups,
+                // so we MUST disable them. But even if we didn't have to,
+                // we would, because nothing in this crate does anything
+                // useful with capturing groups in reverse. And of course,
+                // the lazy DFA ignores capturing groups in all cases.
+                .configure(
+                    thompson_config
+                        .clone()
+                        .which_captures(WhichCaptures::None)
+                        .reverse(true),
+                )
+                .build_many_from_hir(hirs)
+                .map_err(BuildError::nfa)?;
+            let dfa = if !info.config().get_dfa() {
+                Arc::new(wrappers::DFA::none())
             } else {
-                // FIXME: Technically, we don't quite yet KNOW that we need
-                // a reverse NFA. It's possible for the DFAs below to both
-                // fail to build just based on the forward NFA. In which case,
-                // building the reverse NFA was totally wasted work. But...
-                // fixing this requires breaking DFA construction apart into
-                // two pieces: one for the forward part and another for the
-                // reverse part. Quite annoying. Making it worse, when building
-                // both DFAs fails, it's quite likely that the NFA is large and
-                // that it will take quite some time to build the reverse NFA
-                // too. So... it's really probably worth it to do this!
-                let nfarev = thompson::Compiler::new()
-                    // Currently, reverse NFAs don't support capturing groups,
-                    // so we MUST disable them. But even if we didn't have to,
-                    // we would, because nothing in this crate does anything
-                    // useful with capturing groups in reverse. And of course,
-                    // the lazy DFA ignores capturing groups in all cases.
-                    .configure(
-                        thompson_config
-                            .clone()
-                            .which_captures(WhichCaptures::None)
-                            .reverse(true),
-                    )
-                    .build_many_from_hir(hirs)
-                    .map_err(BuildError::nfa)?;
-                let dfa = if !info.config().get_dfa() {
-                    wrappers::DFA::none()
-                } else {
-                    wrappers::DFA::new(&info, pre.clone(), &nfa, &nfarev)
-                };
-                let hybrid = if !info.config().get_hybrid() {
-                    wrappers::Hybrid::none()
-                } else if dfa.is_some() {
-                    debug!("skipping lazy DFA because we have a full DFA");
-                    wrappers::Hybrid::none()
-                } else {
-                    wrappers::Hybrid::new(&info, pre.clone(), &nfa, &nfarev)
-                };
-                (Some(nfarev), hybrid, dfa)
+                Arc::new(wrappers::DFA::new(&info, pre.clone(), &nfa, &nfarev))
             };
+            let hybrid = if !info.config().get_hybrid() {
+                Arc::new(wrappers::Hybrid::none())
+            } else if dfa.is_some() {
+                debug!("skipping lazy DFA because we have a full DFA");
+                Arc::new(wrappers::Hybrid::none())
+            } else {
+                Arc::new(wrappers::Hybrid::new(
+                    &info,
+                    pre.clone(),
+                    &nfa,
+                    &nfarev,
+                ))
+            };
+            (Some(nfarev), hybrid, dfa)
+        };
         Ok(Core {
             info,
             pre,
